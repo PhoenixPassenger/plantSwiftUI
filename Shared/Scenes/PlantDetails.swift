df67e23db3200bcc58d369043fd4aa0375e517a3
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-05-18 13:40:05 -0300

    init of the view to show activities

---
+++ Shared/Scenes/PlantDetails.swift
@@ -0,0 +1,20 @@
+//
+//  PlantDetails.swift
+//  plantatinhas
+//
+//  Created by Rodrigo Silva Ribeiro on 18/05/21.
+//
+
+import SwiftUI
+
+struct PlantDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlantDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantDetails()
+    }
+}
