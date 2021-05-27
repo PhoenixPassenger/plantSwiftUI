88601a820e4ad93e9f1500c0d79ec27135ff112f
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-05-27 12:59:15 -0300

    jsut the notification and the done flag

--- iOS/Components/CustomDatePicker.swift
+++ iOS/Components/CustomDatePicker.swift
@@ -16,11 +16,22 @@ struct CustomDatePicker: View {
     let hourFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
-        formatter.dateFormat = "HH:mm a"
+        formatter.dateFormat = "H:mm a"
         return formatter
     }()
+    func getFinalDate() {
+        let date = dateFormatter.string(from: self.day)
+        let time = hourFormatter.string(from: self.hour)
+        print(time)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy 'at' h:mm a"
+        let string = date + " at " + time                       // "March 24, 2017 at 7:00 AM"
+        let finalDate = dateFormatter.date(from: string)
+        self.date = finalDate ?? Date()
+    }
     @State private var day = Date()
     @State private var hour = Date()
+    @Binding var date: Date
     var body: some View {
         VStack {
             HStack {
@@ -31,18 +42,24 @@ struct CustomDatePicker: View {
                 DatePicker("", selection: $day, in: ...Date(), displayedComponents: .date)
                     .labelsHidden()
                     .accentColor(.fontCreatePlant)
+                    .onChange(of: day) {_ in
+                        getFinalDate()
+                    }
                 DatePicker("", selection: $hour, displayedComponents: .hourAndMinute)
                     .labelsHidden()
                     .accentColor(.fontCreatePlant)
+                    .onChange(of: hour) {_ in
+                        getFinalDate()
+                    }
             }
         }
     }
 }
 
-struct CustomDatePicker_Previews: PreviewProvider {
-    static var previews: some View {
-        ForEach(ColorScheme.allCases, id: \.self) {
-            CustomDatePicker().preferredColorScheme($0)
-        }
-    }
-}
+//struct CustomDatePicker_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ForEach(ColorScheme.allCases, id: \.self) {
+//            CustomDatePicker().preferredColorScheme($0)
+//        }
+//    }
+//}
--- iOS/scenes/NewPlantForm/NewPlantForm.swift
+++ iOS/scenes/NewPlantForm/NewPlantForm.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Combine
+
 
 class PlantViewModel: ObservableObject {
     // Plant Details
@@ -21,31 +23,71 @@ class PlantViewModel: ObservableObject {
     @Published var compostExist = false
     @Published var compostNotification = false
     @Published var compostDay: Date = Date()
-    @Published var compostInterval = 0
+    @Published var compostInterval = 1
     @Published var compostIsMonthly = false
     // Harvest
     @Published var harvestDone = false
     @Published var harvestExist = false
     @Published var harvestNotification = false
     @Published var harvestDay: Date = Date()
-    @Published var harvestInterval = 0
+    @Published var harvestInterval = 1
     @Published var harvestIsMonthly = false
+    
+    
+    let onSave = PassthroughSubject<Void, Never>()
+    let onCancel = PassthroughSubject<Void, Never>()
+    
+    
+    func saveInCoreData() {
+        let plantWorker = PlantWorker()
+        let plant = plantWorker.create(plant: PlantModel(name: name,
+                                                         disease: false,
+                                                         profilePhoto: name))
+        var waterWorker = WaterWorker(plant)
+        var harvestWorker = HarvestWorker(plant)
+        var fertilizeWorker = FertilizeWorker(plant)
+
+        waterWorker.create(water: WaterModel(exist: waterExist,
+                                             notification: waterNotification,
+                                             done: waterDone,
+                                             date: Date()))
+        harvestWorker.create(harvest: HarvestModel(exist: harvestExist,
+                                                   notification: harvestNotification,
+                                                   done: harvestDone,
+                                                   date: harvestDay,
+                                                   interval: harvestInterval,
+                                                   isMonthly: harvestIsMonthly))
+        fertilizeWorker.create(fertilize: FertilizeModel(exist: compostExist,
+                                                         notification: compostNotification,
+                                                         done: compostDone,
+                                                         date: compostDay,
+                                                         interval: compostInterval,
+                                                         isMonthly: compostIsMonthly))
+        
+        onSave.send()
+    }
+    
 }
 
 struct NewPlantForm: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject var viewModel = PlantViewModel()
+    
     @Binding var showModal: Bool
     var body: some View {
         NavigationView {
-            FirstPage(showModal: $showModal)
-        }.environmentObject(viewModel)
+            FirstPage()
+        }
+        .environmentObject(viewModel)
+        .onReceive(viewModel.onSave) {
+            showModal = false
+        }
     }
 }
 
 struct FirstPage: View {
     @EnvironmentObject private var viewModel: PlantViewModel
-    @Binding var showModal: Bool
+    
     var body: some View {
         VStack {
             HStack {
@@ -58,28 +100,28 @@ struct FirstPage: View {
             ImagePickerView(imageToShare: $viewModel.image)
             Spacer()
         }.foregroundColor(.black)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                NavigationLink(destination: SecondPage(showModal: $showModal)) {
-                    Text("Próximo")
-                }.environmentObject(viewModel)
-            }
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button("Fechar") {
-                    self.showModal.toggle()
-                }
-            }
-        }
         .navigationBarTitle("Title View2", displayMode: .inline)
         .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: leadingButton, trailing: trailingButton)
     }
+    var leadingButton: some View {
+        Button("Fechar") {
+            viewModel.onCancel.send()
+        }
+    }
+    
+    var trailingButton: some View {
+        NavigationLink(destination: SecondPage()) {
+            Text("Próximo")
+        }
+    }
+    
 }
 
 struct SecondPage: View {
     @EnvironmentObject private var viewModel: PlantViewModel
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var isOn = false
-    @Binding var showModal: Bool
     var backButton : some View { Button(action: {
         self.presentationMode.wrappedValue.dismiss()
     }) {
@@ -105,9 +147,9 @@ struct SecondPage: View {
             }.foregroundColor(.black)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: ThirdPage(showModal: $showModal)) {
+                    NavigationLink(destination: ThirdPage()) {
                         Text("Próximo")
-                    }.environmentObject(viewModel)
+                    }
                 }
             }
         }
@@ -121,7 +163,6 @@ struct ThirdPage: View {
     @EnvironmentObject private var viewModel: PlantViewModel
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var isOn = false
-    @Binding var showModal: Bool
 
     var backButton : some View { Button(action: {
         self.presentationMode.wrappedValue.dismiss()
@@ -143,15 +184,15 @@ struct ThirdPage: View {
                               diseaseIndicator: .compost,
                               notificationBool: $viewModel.compostExist,
                               label: "Adubação")
-                SegmentedControl()
-                CustomDatePicker()
+                SegmentedControl(isMonthly: $viewModel.compostIsMonthly, timeCount: $viewModel.compostInterval)
+                CustomDatePicker(date: $viewModel.compostDay)
                 Spacer()
             }.foregroundColor(.black)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: FourthPage(showModal: $showModal)) {
+                    NavigationLink(destination: FourthPage()) {
                         Text("Próximo")
-                    }.environmentObject(viewModel)
+                    }
                 }
             }
         }
@@ -166,7 +207,6 @@ struct FourthPage: View {
     @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var isOn = false
-    @Binding var showModal: Bool
 
     var backButton : some View { Button(action: {
         self.presentationMode.wrappedValue.dismiss()
@@ -188,16 +228,15 @@ struct FourthPage: View {
                               diseaseIndicator: .compost,
                               notificationBool: $viewModel.harvestExist,
                               label: "Colheita")
-                SegmentedControl()
-                CustomDatePicker()
+                SegmentedControl(isMonthly: $viewModel.harvestIsMonthly, timeCount: $viewModel.harvestInterval)
+                CustomDatePicker(date: $viewModel.compostDay)
                 Spacer()
             }.foregroundColor(.black)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Salvar") {
-                        saveInCoreData()
-                        self.showModal.toggle()
-                    }.environmentObject(viewModel)
+                        viewModel.saveInCoreData()
+                    }
                 }
             }
         }
